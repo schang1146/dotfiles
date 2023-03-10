d925307a81c30e50d64f083455649f49131f1744
schang1146 <20074034+schang1146@users.noreply.github.com>
2023-03-10 15:58:48 -0800

    add install.sh for zsh
    should probabably separate oh-my-zsh out

---
+++ zsh/install.sh
@@ -0,0 +1,6 @@
+# zsh
+sudo apt install zsh
+
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
