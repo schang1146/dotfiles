9685ca8ea693f344b5c74ed779ecb92aa11d166c
schang1146 <20074034+schang1146@users.noreply.github.com>
2023-03-11 15:00:16 -0800

    move oh-my-zsh install steps

---
+++ omz/install.sh
@@ -0,0 +1,5 @@
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# oh-my-zsh plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
--- zsh/install.sh
+++ zsh/install.sh
@@ -1,6 +1,2 @@
 # zsh
 sudo apt install zsh
-
-# oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
