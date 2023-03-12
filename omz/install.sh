48d4b9d5474f59c86e4b0e8ce3a60a5559aad6cd
schang1146 <20074034+schang1146@users.noreply.github.com>
2023-03-11 16:03:55 -0800

    remove oh-my-zsh install.sh file

--- omz/install.sh
+++
@@ -1,5 +0,0 @@
-# oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# oh-my-zsh plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
