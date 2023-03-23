361a6c427d54e0ef872c28d24512909252abbefc
schang1146 <20074034+schang1146@users.noreply.github.com>
2023-03-23 15:27:51 -0700

    add plugins

---
+++ zsh/plugins.zsh
@@ -0,0 +1,6 @@
+# https://github.com/zsh-users/zsh-autosuggestions
+zsh_add_plugin zsh-users/zsh-autosuggestions
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=magenta'
+
+# https://github.com/zsh-users/zsh-syntax-highlighting
+zsh_add_plugin zsh-users/zsh-syntax-highlighting
