b54618150dca009db65602b2a7812a93d3c9d76d
schang1146 <20074034+schang1146@users.noreply.github.com>
2023-03-24 23:14:43 -0700

    comment out zsh-syntax-highlighting
    not a fan of the colors and formatting for now

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -3,4 +3,4 @@ zsh_add_plugin zsh-users/zsh-autosuggestions
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=magenta'
 
 # https://github.com/zsh-users/zsh-syntax-highlighting
-zsh_add_plugin zsh-users/zsh-syntax-highlighting
+# zsh_add_plugin zsh-users/zsh-syntax-highlighting
