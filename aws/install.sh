23b5f21c0ef68b5bf2d70d01169288dfe2756f24
schang1146 <20074034+schang1146@users.noreply.github.com>
2024-02-10 15:58:38 -0800

    change directories before installing aws-cli

--- aws/install.sh
+++ aws/install.sh
@@ -5,7 +5,10 @@ if ! which unzip;
 then
     sudo apt install unzip
 fi
+
+cd ~
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
 rm -r ./aws
+cd -
